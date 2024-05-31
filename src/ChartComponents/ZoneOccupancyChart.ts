454d20537a3808af0782526c57b60c455214d659
dariush-hassani <dariush.hassani@outlook.com>
2024-05-31 19:13:45 +0330

    peaks done

--- src/ChartComponents/ZoneOccupancyChart.ts
+++ src/ChartComponents/ZoneOccupancyChart.ts
@@ -12,7 +12,7 @@ class ZoneOccupancyChart {
   };
 
   public config = {
-    EmptySpaceBetweenBars: 12, //(px)
+    EmptySpaceBetweenBars: 50, //(px)
     marginTop: 20,
     marginLeft: 40,
     marginBottom: 0,
@@ -30,8 +30,8 @@ class ZoneOccupancyChart {
   private _height: number;
   private _chartWidth: number;
   private _chartHeight: number;
-  private _xScaleFunction: any;
-  private _yScaleFunction: any;
+  private _xScaleFunction: d3.ScaleBand<string> = d3.scaleBand();
+  private _yScaleFunction: d3.ScaleLinear<number, number> = d3.scaleLinear();
 
   public constructor(
     id: string,
@@ -72,11 +72,15 @@ class ZoneOccupancyChart {
 
     let allCapacities = this._data.map((x) => x.Capacity);
     let allPeaks = this._data.map((x) => x.Peak);
-    let maxCapacitiesAndPeaks = d3.max([...allPeaks, ...allCapacities]);
+    let maxCapacitiesAndPeaks = d3.max([
+      ...allPeaks,
+      ...allCapacities,
+    ]) as number;
+    maxCapacitiesAndPeaks = maxCapacitiesAndPeaks + maxCapacitiesAndPeaks * 0.2;
 
     this._yScaleFunction = d3
       .scaleLinear()
-      .domain([maxCapacitiesAndPeaks as number, 0])
+      .domain([maxCapacitiesAndPeaks, 0])
       .range([
         0,
         this._chartHeight -
@@ -117,7 +121,7 @@ class ZoneOccupancyChart {
     let yAxis = d3
       .axisRight(this._yScaleFunction)
       .tickSize(tickSize)
-      .ticks(this._chartHeight / 100);
+      .ticks(this._chartHeight / 50);
     d3.select(`#${this._id} svg`)
       .append("g")
       .attr("class", "yAxis")
@@ -143,15 +147,152 @@ class ZoneOccupancyChart {
       .style("stroke-width", `2px`);
   }
 
+  private DrawBars(barType: "Capacity" | "Average"): void {
+    let bandwidth = this._xScaleFunction.bandwidth();
+    let barWidth = bandwidth - this.config.EmptySpaceBetweenBars;
+
+    d3.select(`#${this._id} svg`)
+      .selectAll()
+      .data(this._data)
+      .enter()
+      .append("rect")
+      .attr(
+        "x",
+        (d, i) =>
+          this.config.paddingLeft + (i + 0.5) * bandwidth - barWidth / 2,
+      )
+      .attr("width", barWidth)
+      .attr(
+        "y",
+        (d, i) => this._yScaleFunction(d[barType]) + this.config.paddingTop,
+      )
+      .attr(
+        "height",
+        (d, i) =>
+          this._chartHeight -
+          (this.config.paddingBottom + this.config.paddingTop) -
+          this._yScaleFunction(d[barType]),
+      )
+      .attr(
+        "fill",
+        barType === "Capacity"
+          ? this.colorPalette.CapacityColor
+          : this.colorPalette.AverageColor,
+      )
+      .attr(
+        "class",
+        (d, i) => `bar-${barType === "Average" ? "avg" : "cap"}-${i}`,
+      )
+      .attr("rx", 7)
+      .attr("ry", 7);
+  }
+
+  private ModifyBottomRadius(): void {
+    for (let i = 0; i < this._data.length; i++) {
+      let avgHeightStr: string | null | undefined = document
+        .querySelector(`#${this._id} svg .bar-avg-${i}`)
+        ?.getAttribute("height");
+      let avgHeight = avgHeightStr ? parseInt(avgHeightStr) : 0;
+
+      let capHeightStr: string | null | undefined = document
+        .querySelector(`#${this._id} svg .bar-cap-${i}`)
+        ?.getAttribute("height");
+      let capHeight = capHeightStr ? parseInt(capHeightStr) : 0;
+
+      let rectHeight;
+      let rectColor;
+      let barType;
+      if (avgHeight > 5) {
+        rectHeight = 5;
+        rectColor = this.colorPalette.AverageColor;
+        barType = "avg";
+      } else if (avgHeight) {
+        rectHeight = avgHeight;
+        rectColor = this.colorPalette.AverageColor;
+        barType = "avg";
+      } else if (capHeight > 5) {
+        rectHeight = 5;
+        rectColor = this.colorPalette.CapacityColor;
+        barType = "cap";
+      } else if (capHeight) {
+        rectHeight = capHeight;
+        rectColor = this.colorPalette.CapacityColor;
+        barType = "cap";
+      } else {
+        //nothing
+      }
+      if (rectHeight && rectColor && barType) {
+        let refRect = document.querySelector(
+          `#${this._id} svg .bar-${barType}-${i}`,
+        ) as HTMLElement;
+        d3.select(`#${this._id} svg`)
+          .append("rect")
+          .attr("x", refRect.getAttribute("x"))
+          .attr(
+            "y",
+            this._chartHeight - (this.config.paddingBottom + rectHeight),
+          )
+          .attr("width", refRect.getAttribute("width"))
+          .attr("height", rectHeight)
+          .attr("fill", rectColor);
+      }
+    }
+  }
+
+  private DrawPeaks(): void {
+    let bandwidth = this._xScaleFunction.bandwidth();
+    let barWidth = bandwidth - this.config.EmptySpaceBetweenBars;
+
+    d3.select(`#${this._id} svg`)
+      .selectAll()
+      .data(this._data)
+      .enter()
+      .append("line")
+      .attr(
+        "x1",
+        (d, i) =>
+          this.config.paddingLeft + (i + 0.5) * bandwidth - barWidth / 2 - 3,
+      )
+      .attr(
+        "x2",
+        (d, i) =>
+          this.config.paddingLeft +
+          (i + 0.5) * bandwidth -
+          barWidth / 2 +
+          barWidth +
+          3,
+      )
+      .attr("width", barWidth)
+      .attr(
+        "y1",
+        (d, i) => this._yScaleFunction(d.Peak) + this.config.paddingTop,
+      )
+      .attr(
+        "y2",
+        (d, i) => this._yScaleFunction(d.Peak) + this.config.paddingTop,
+      )
+      .attr("stroke", this.colorPalette.PeakColor)
+      .attr("stroke-width", "2px")
+      .attr("stroke-dasharray", "5");
+  }
+
   //public methods
-  public DrawChart() {
+  public DrawChart(): void {
     this.SetupLayout();
     this.SetupScaleFunctions();
     this.SetupXAxis();
     this.SetupYAxis();
+    this.DrawBars("Capacity");
+    this.DrawBars("Average");
+    this.ModifyBottomRadius();
+    this.DrawPeaks();
   }
 
   public ChangeDimension(width: number, height: number): void {
+    this._chartWidth =
+      width - (this.config.marginLeft + this.config.marginRight);
+    this._chartHeight =
+      height - (this.config.marginBottom + this.config.marginTop);
     this._width = width;
     this._height = height;
     this.DrawChart();
